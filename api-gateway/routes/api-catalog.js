fd46c3225a6472a709c51d8f7012624a1b9636da
Timothy Wilson <tiwilson@my.bellevue.edu>
2018-12-08 20:38:08 -0600

    Adding assignment 8.4

--- api-gateway/routes/api-catalog.js
+++ api-gateway/routes/api-catalog.js
@@ -3,6 +3,7 @@
  */
 
 var express = require('express');
+var checkToken = require('../check-token');
 var router = express.Router();
 
 
@@ -14,7 +15,7 @@ router.post('/auth/register', auth_controller.user_register);
 
 
 // GET request for verifying user tokens
-router.get('/auth/token', auth_controller.user_token);
+router.get('/auth/token', checkToken, auth_controller.user_token);
 
 
 // POST request for signing users in
@@ -25,4 +26,4 @@ router.post('/auth/login', auth_controller.user_login);
 router.get('/auth/logout', auth_controller.user_logout);
 
 
-module.exports = router;
+module.exports = router; 
